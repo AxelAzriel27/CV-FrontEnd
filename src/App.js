4c4b653c2b0ade7efeb55aa58e8a88ace545b6c5
AxelAzriel27 <4xelkopalit@gmail.com>
2024-11-08 09:30:07 +0800

    adding certification components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./assets/css/style.css";
+import Certification from "./components/certification";
 import Education from "./components/education";
 import Header from "./components/header";
 import Profile from "./components/profile";
@@ -12,25 +13,7 @@ const App = () => {
         <Profile />
         <Education />
         <Skills />
-        <section>
-          <div className="sectionTitle">
-            <h1>Certifications</h1>
-          </div>
-          <div className="sectionContent">
-            <article>
-              <h2>Job Title at Company</h2>
-              <p className="subDetails">April 2011 - Present</p>
-              <img src="../IMG_11701.png" className="certificate" alt="" />
-            </article>
-            <br />
-            <article>
-              <h2>Job Title at Company</h2>
-              <p className="subDetails">Janruary 2007 - March 2011</p>
-              <img src="../IMG_11701.png" className="certificate" alt="" />
-            </article>
-          </div>
-          <div className="clear" />
-        </section>
+        <Certification />
       </div>
     </div>
   );
---
+++ src/components/certification/index.js
@@ -0,0 +1,25 @@
+const Certification = () => {
+  return (
+    <section>
+      <div className="sectionTitle">
+        <h1>Certifications</h1>
+      </div>
+      <div className="sectionContent">
+        <article>
+          <h2>Job Title at Company</h2>
+          <p className="subDetails">April 2011 - Present</p>
+          <img src="../IMG_11701.png" className="certificate" alt="" />
+        </article>
+        <br />
+        <article>
+          <h2>Job Title at Company</h2>
+          <p className="subDetails">Janruary 2007 - March 2011</p>
+          <img src="../IMG_11701.png" className="certificate" alt="" />
+        </article>
+      </div>
+      <div className="clear" />
+    </section>
+  );
+};
+
+export default Certification;
